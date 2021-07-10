44ec01b3173be50a0070db211b79c8af14a5cb7f
Karim-khadro <57905880+Karim-khadro@users.noreply.github.com>
2021-07-10 14:10:14 +0200

    v1- works correctly

--- CreateNewProject.py
+++ CreateNewProject.py
@@ -7,10 +7,17 @@ import os
 import requests
 import json
 
+from requests.api import head
+
 load_dotenv()
 
-#TODO : Comments and ReadME
-#TODO : TESTING
+# TODO : Comments and ReadME
+# TODO : Fast mode
+# TODO : Add to windows
+# TODO : Save profile
+# TODO : Add help
+# TODO : TESTING
+
 
 # * Printing style for the questions
 style = style_from_dict({
@@ -26,22 +33,22 @@ style = style_from_dict({
 # *  Check:
 # *      If the git ignore file exists in the templates
 # *      That only one gitignore file selected
+
 class GitIgnoreValidator(Validator):
     def validate(self, document):
-        l = []
-        pathTemplate = os.path.join(os.getcwd(), "Gitignore_Template")
-        for file in os.listdir(pathTemplate):
-            if file.lower().startswith(document.text.lower()):
-                l.append(file)
-        if document.text == "":
-            raise ValidationError(
-                message='Please enter some letters to see the propositions',
-                cursor_position=len(document.text))  # Move cursor to end
-        elif len(l) > 1 or len(l) == 0:
-            raise ValidationError(
-                message=f'Please enter more letters. Some suggestions : {l[0:5]}',
-                cursor_position=len(document.text))  # Move cursor to end
+        if document.text != "":
+            gitignoreUrl = os.getenv("URLGITIGNORE")
+            language = document.text
+            language = language.lower()
+            language = language[0].upper() + language[1:]
 
+            gitignoreUrl = gitignoreUrl + language
+            r = requests.get(gitignoreUrl)
+            if r.status_code == 404:
+                raise ValidationError(
+                    message='Please enter a valide language name. To see all valide names: https://api.github.com/gitignore/templates ',
+                    cursor_position=len(document.text))  # Move cursor to end
+        
 
 folderName = str(sys.argv[1])
 
@@ -79,68 +86,46 @@ questions = [
     {
         'type': 'input',
         'name': 'gitignoreLanguage',
-        'message': 'Which language?',
+        'message': 'Which language? Empty: no gitignore',
         'validate': GitIgnoreValidator,
         'when': lambda answers: answers.get('gitignore', True)
-    },
-    {
-        'type': 'list',
-        'name': 'gitMethod',
-        'message': 'Which method?[default value : clone',
-        'choices': ['Clone', 'Remote'],
-        'filter': lambda val: val.lower()
-    },
-    {
-        'type': 'input',
-        'name': 'remoteName',
-        'message': 'Remote name?[defalut value : main]',
-        'default': 'main',
-        'when': lambda answers: answers.get('gitMethod') == 'remote'
     }
+    # ,{
+    #     'type': 'list',
+    #     'name': 'gitMethod',
+    #     'message': 'Which method?[default value : clone',
+    #     'choices': ['Clone', 'Remote'],
+    #     'filter': lambda val: val.lower()
+    # },
+    # {
+    #     'type': 'input',
+    #     'name': 'remoteName',
+    #     'message': 'Remote name?[defalut value : main]',
+    #     'default': 'main',
+    #     'when': lambda answers: answers.get('gitMethod') == 'remote'
+    # }
 ]
 answers = prompt(questions, style=style)
-
-# * Put the full name of git ignore template
-if answers["gitignore"]:
-    pathTemplate = os.path.join(os.getcwd(), "Gitignore_Template")
-    for file in os.listdir(pathTemplate):
-        if file.lower().startswith(answers["gitignoreLanguage"].lower()):
-            answers["gitignoreLanguage"] = file
-
 
 # Create repo on github
 token = os.getenv("TOKEN")
 query_url = os.getenv("URL")
 homePage = os.getenv("HOMEPAGE")
 headers = {'Authorization': f'token {token}'}
-if answers["gitMethod"] == "clone":
-    if answers["gitignore"]:
-        data = {
-            "name": folderName,
-            "homepage": homePage,
-            "private": answers["private"],
-            "has_issues": answers["issues"],
-            "has_wiki": answers["wiki"],
-            "gitignore_template": answers["gitignoreLanguage"],
-            "auto_init": answers["readme"]
-        }
-    else:
-        data = {
-            "name": folderName,
-            "homepage": homePage,
-            "private": answers["private"],
-            "has_issues": answers["issues"],
-            "has_wiki": answers["wiki"],
-            "gitignore_template": False,
-            "auto_init": answers["readme"]
-        }
-    r = requests.post(query_url, headers=headers, data=json.dumps(data))
-    rsp = r.json()
-    print(rsp)
-    git_url = rsp["clone_url"]
-    commands = [f'git clone {git_url}']
-    for c in commands:
-        os.system(c)
+# if answers["gitMethod"] == "clone":
+if answers["gitignore"] and answers["gitignoreLanguage"] != "":
+    language = answers["gitignoreLanguage"]
+    language = language.lower()
+    language = language[0].upper() + language[1:]
+    data = {
+        "name": folderName,
+        "homepage": homePage,
+        "private": answers["private"],
+        "has_issues": answers["issues"],
+        "has_wiki": answers["wiki"],
+        "gitignore_template": language,
+        "auto_init": answers["readme"]
+    }
 
 else:
     data = {
@@ -148,29 +133,49 @@ else:
         "homepage": homePage,
         "private": answers["private"],
         "has_issues": answers["issues"],
-        "has_wiki": answers["wiki"]
+        "has_wiki": answers["wiki"],
+        "gitignore_template": False,
+        "auto_init": answers["readme"]
     }
 
-    r = requests.post(query_url, headers=headers, data=json.dumps(data))
-    rsp = r.json()
-    print(rsp)
-    git_url = rsp["clone_url"]
-    os.mkdir(folderName)
-    os.chdir(folderName)
-    commands = [
-                'git init',
-                f'git remote add origin {git_url}',
-                'git add .',
-                'git commit -m "Initial commit"',
-                f'git push -u origin {answers["remoteName"]}']
-    if answers["readme"]:
-        commands.insert(0,f'echo "# {folderName}" >> README.md')
-    if answers["gitignore"]:
-        url = os.getenv("URLGITIGNOR")+answers["gitignoreLanguage"].split(".gitignore")[0]
-        rget = requests.get(url)
-        rsp = rget.json()
-        open(".gitignore", 'wb').write(bytes(rsp["source"],encoding='utf8'))
-       
-    print(commands)
-    for c in commands:
-        os.system(c)
+r = requests.post(query_url, headers=headers, data=json.dumps(data))
+rsp = r.json()
+print(rsp)
+git_url = rsp["clone_url"]
+commands = [f'git clone {git_url}']
+for c in commands:
+    os.system(c)
+
+# else:
+#     data = {
+#         "name": folderName,
+#         "homepage": homePage,
+#         "private": answers["private"],
+#         "has_issues": answers["issues"],
+#         "has_wiki": answers["wiki"]
+#     }
+
+#     r = requests.post(query_url, headers=headers, data=json.dumps(data))
+#     rsp = r.json()
+#     print(rsp)
+#     git_url = rsp["clone_url"]
+#     os.mkdir(folderName)
+#     os.chdir(folderName)
+#     commands = [
+#                 'git init',
+#                 f'git remote add origin {git_url}',
+#                 'git add .',
+#                 'git commit -m "Initial commit"',
+#                 f'git push -u origin {answers["remoteName"]}']
+#     if answers["readme"]:
+#         commands.insert(0,f'echo "# {folderName}" >> README.md')
+#     if answers["gitignore"]:
+#         url = str(os.getenv("URLGITIGNOR"))+str(answers["gitignoreLanguage"]).split(".gitignore")[0]
+#         print(url)
+#         rget = requests.get(url)
+#         rsp = rget.json()
+#         open(".gitignore", 'wb').write(bytes(rsp["source"],encoding='utf8'))
+
+#     print(commands)
+#     for c in commands:
+#         os.system(c)
